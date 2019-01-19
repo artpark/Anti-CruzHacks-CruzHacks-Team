a9415783ea7f0f814b65d57d6b848c1a67d5a803
Thanut Art Parkeenvincha <art.parkeenvincha@gmail.com>
2019-01-18 21:22:42 -0800

    Added Note Prefab

---
+++ Anti CruzHacks CruzHacks Team/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject note;
+    public Transform noteSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space pressed");
+            GameObject newNote = (GameObject)Instantiate(note, note.transform.position, note.transform.rotation);
+            newNote.GetComponent<Rigidbody>().velocity = newNote.transform.forward * -6.0f;
+        }
+    }
+}
