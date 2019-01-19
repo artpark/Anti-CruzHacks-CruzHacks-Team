c8417f14df7d89c19e849ba0c22a69e9e8ced869
Jesse Cirimelli-Low <jcirimel@ucsc.edu>
2019-01-19 12:40:20 -0800

    multitrack drifting

--- Anti CruzHacks CruzHacks Team/Assets/Scripts/GameController.cs
+++ Anti CruzHacks CruzHacks Team/Assets/Scripts/GameController.cs
@@ -1,32 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MidiPlayerTK;
 
 public class GameController : MonoBehaviour
 {
     public GameObject note;
     public Transform noteSpawn;
-
+    public Material Note;
+    public Material Note_1;
+    public Material Note_2;
+//    public MidiFilePlayer midiFilePlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.frameCount % 90 == 0)
-        {
-            Debug.Log("New note created");
-            NewNote(6);
-            //Destroy (newNote, 5f);            
-        }
+      //  if(Time.frameCount % 90 == 0)
+      //  {
+      //      Debug.Log("New note created");
+      //      NewNote(6);
+            //Destroy (newNote, 5f);
+
     }
 
-    void NewNote(float speed) //normal speed is 6
+
+    public void NewNote(float speed) //normal speed is 6
     {
+      //int doublenote = Random.Range(0,3);
+
+        int bar = Random.Range(0, 3);
+
+        //Debug.Log(bar);
+        float track = (float) (-3.5 + (3.5 * bar));
+
+        if (bar == 0){
+            note.GetComponent<Renderer>().material = Note_2;
+        }
+        if (bar == 1){
+            note.GetComponent<Renderer>().material = Note_1;
+        }
+        if (bar == 2){
+            note.GetComponent<Renderer>().material = Note;
+
+        }
+
+        note.transform.position = new Vector3(track,0,14);
         GameObject newNote = (GameObject)Instantiate(note, note.transform.position, note.transform.rotation);
         newNote.GetComponent<Rigidbody>().velocity = newNote.transform.forward * -speed;
+
+
     }
 }
