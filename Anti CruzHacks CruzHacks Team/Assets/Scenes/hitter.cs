9c3986c88308b9b7c718cc8f789bfb1988e93004
Terry <terryguan1@gmail.com>
2019-01-19 01:17:38 -0800

    added win loss conditions

---
+++ Anti CruzHacks CruzHacks Team/Assets/Scenes/hitter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitter : MonoBehaviour
+{
+    private float collision;
+    private float xPosition;
+    private float yPosition;
+    private float zPosition;
+    private Vector2 spherePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementTowardsCamera sphere = new movementTowardsCamera();
+        xPosition = sphere.getX();
+        yPosition = sphere.getY() - 10;
+        zPosition = sphere.getZ();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spherePosition = new Vector3(xPosition, yPosition, zPosition);
+        transform.position = spherePosition;
+    }
+}
---
+++ Anti CruzHacks CruzHacks Team/Assets/Scenes/movementTowardsCamera.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementTowardsCamera : MonoBehaviour
+{
+    private Vector2 spherePosition;
+    private float xMovePosition = 0f;
+    private float yMovePosition = 7f;
+    private float zMovePosition = 0f;
+    private float xOriginalPosition = 0f;
+    private float yOriginalPosition = 7f;
+    private float zOriginalPosition = 0f;
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+    public float getX()
+    {
+        return xMovePosition;
+    }
+    public float getY()
+    {
+        return yMovePosition;
+    }
+    public float getZ()
+    {
+        return zMovePosition;
+    }
+    public void makeNew()
+    {
+        Instantiate(prefab, new Vector3(xOriginalPosition, yOriginalPosition, zOriginalPosition), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yMovePosition = yMovePosition - .08f;
+        spherePosition = new Vector3(xMovePosition, yMovePosition, zMovePosition);
+        transform.position = spherePosition;
+
+        //Touch touch = new Touch();
+        //print(touch.position);
+        if (Input.GetButtonDown("Fire1") && yMovePosition >= -3.5 && yMovePosition <= -2.5)
+        {
+            print("winner winner chicken dinner");
+            makeNew();
+            Destroy(this.gameObject);
+        }
+
+        if (Input.GetButtonDown("Fire1") && (yMovePosition <= -3.5 || yMovePosition >= -2.5))
+        {
+            print("loser loser you're a loser");
+            makeNew();
+            Destroy(this.gameObject);
+        }
+
+
+        if (yMovePosition < -8)
+        {
+            makeNew();
+            print("loser loser you're a loser");
+            Destroy(this.gameObject);
+        }
+    }
+
+}
