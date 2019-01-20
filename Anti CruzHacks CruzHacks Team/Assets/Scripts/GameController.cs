60b84399bb3f5a791eb72fbc0f0b30d1f37157f7
Thanut Art Parkeenvincha <art.parkeenvincha@gmail.com>
2019-01-19 21:11:23 -0800

    Goodbye CruzHacks

--- Anti CruzHacks CruzHacks Team/Assets/Scripts/GameController.cs
+++ Anti CruzHacks CruzHacks Team/Assets/Scripts/GameController.cs
@@ -7,33 +7,47 @@ public class GameController : MonoBehaviour
 {
     public GameObject note;
     public Transform noteSpawn;
-    public Text coordinates;
+    public Material Note;
+    public Material Note_1;
+    public Material Note_2;
 
     // Start is called before the first frame update
     void Start()
     {
-        coordinates.text = "Touch Position : None";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.frameCount % 45 == 0)
-        {
-            Debug.Log("New note created");
-            NewNote(10);
-            //Destroy (newNote, 5f);            
-        }
-        // if(Input.touchCount > 0)
+        // if(Time.frameCount % 45 == 0)
         // {
-        //     Touch touch = Input.GetTouch(0);
-        //     coordinates.text = "Touch Position : " + touch.position.x + " " + touch.position.y;
+        //     Debug.Log("New note created");
+        //     NewNote(10);
+        //     //Destroy (newNote, 5f);            
         // }
     }
 
-    void NewNote(float speed) //normal speed is 6
+    public void NewNote(float speed) //normal speed is 6
     {
+        int bar = Random.Range(0, 3);
+
+        //Debug.Log(bar);
+        float track = (float) (-1 + (1 * bar));
+
+        if (bar == 0){
+            note.GetComponent<Renderer>().material = Note_2;
+        }
+        if (bar == 1){
+            note.GetComponent<Renderer>().material = Note_1;
+        }
+        if (bar == 2){
+            note.GetComponent<Renderer>().material = Note;
+
+        }
+
+        note.transform.position = new Vector3(track,0,14);
         GameObject newNote = (GameObject)Instantiate(note, note.transform.position, note.transform.rotation);
         newNote.GetComponent<Rigidbody>().velocity = newNote.transform.forward * -speed;
+
     }
 }
